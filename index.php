6e9cdc9dcbad0f3651d50e48448fdab4028f1394
frame88 <fleuzzi97@gmail.com>
2022-02-09 21:42:19 +0100

    consegna

---
+++ index.php
@@ -0,0 +1,12 @@
+<!-- Esercizio di oggi:
+cartella/repo php-ajax-dischi
+Stampare a schermo una decina di dischi musicali
+Prima Milestone:
+Stampiamo i dischi solo con l'utilizzo di PHP, che stampa direttamente i dischi in pagina:
+ al caricamento della pagina ci saranno tutti i dischi.
+
+Seconda Milestone:
+Attraverso l'utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api,
+ i dischi a php e li stamperà attraverso vue. -->
+
+ 
