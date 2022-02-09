3fa27f536fb28ee653c8e2546e1171a4bd554540
frame88 <fleuzzi97@gmail.com>
2022-02-09 21:47:48 +0100

    header

--- index.php
+++ index.php
@@ -9,4 +9,19 @@ Seconda Milestone:
 Attraverso l'utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api,
  i dischi a php e li stamperà attraverso vue. -->
 
- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>dischi php</title>
+</head>
+<body>
+    <?php include_once __DIR__ . '/partials/header.php' ?>
+
+</body>
+</html> 
---
+++ partials/header.php
@@ -0,0 +1,3 @@
+<header>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+</header> 
